ccb04ed7d4b7c52e8b5e6be8932bf04a872c7f34
leprox43 <leprox43@gmail.com>
2026-06-06 18:39:36 +0200

    [fix]

--- custom_components/delonghi_primadonna/switch.py
+++ custom_components/delonghi_primadonna/switch.py
@@ -190,7 +190,7 @@ class DelongiPrimadonnaTimeSyncSwitch(
 
     def turn_on(self, **kwargs: Any) -> None:
         """Turn the sounds on."""
-        self.hass.async_create_task(self.device.set_time(datetime.now()))
+        self.hass.async_create_task(self.device.set_time(datetime.datetime.now()))
         self._attr_is_on = True
 
     def turn_off(self, **kwargs: Any) -> None:
